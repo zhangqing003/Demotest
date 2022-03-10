9bb901e2527e26031dcafc1708fc9af12d3f8ec6
zhangqing <251265227@qq.com>
2022-03-10 20:07:43 +0800

    master 第一次提交

--- Project1/Project1/源.cpp
+++ Project1/Project1/源.cpp
@@ -6,13 +6,12 @@ using namespace std;
 int main() {
 	vector<int >a;
 	map<int, int> s;
+	vector<int>b(a);
 	s.insert(make_pair(1, 2));
 	s.insert(make_pair(2, 2));
 	for (auto it = s.begin();  it != s.end(); it++) {
 
 		cout << it->first << "   " << it->second << endl;
 	}
-	
-
-
+	cout << "hhh" << endl;
 }
