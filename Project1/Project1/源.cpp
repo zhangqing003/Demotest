49cbd3a665af0aa118e0793c3d510b3bdce67d7a
zhangqing <251265227@qq.com>
2022-03-10 18:09:14 +0800

    第一次提交

---
+++ Project1/Project1/源.cpp
@@ -0,0 +1,18 @@
+#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+int main() {
+	vector<int >a;
+	map<int, int> s;
+	s.insert(make_pair(1, 2));
+	s.insert(make_pair(2, 2));
+	for (auto it = s.begin();  it != s.end(); it++) {
+
+		cout << it->first << "   " << it->second << endl;
+	}
+	
+
+
+}
